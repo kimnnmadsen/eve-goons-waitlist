578a9d8911811e063fad6ab050d109a6360d2cf0
Kim Madsen <kimnnmadsen@gmail.com>
2020-04-08 15:00:44 +0200

    fixed port

--- setup.js
+++ setup.js
@@ -1,7 +1,7 @@
 exports.oauth = {
         clientID: "40a82d63f8b14174b43dd076d347453a",
         secretKey: "IpaMeqTpKsPuBntqx5xogOyh7dWk9oAl9kQ9KU7v",
-        callbackURL: "https://tdf.n3it.xyz:1888/auth/provider/callback",
+        callbackURL: "https://tdf.n3it.xyz:5000/auth/provider/callback",
         scopes: ['publicData','esi-location.read_location.v1','esi-location.read_ship_type.v1','esi-skills.read_skills.v1','esi-skills.read_skillqueue.v1','esi-fleets.read_fleet.v1','esi-fleets.write_fleet.v1','esi-ui.open_window.v1','esi-ui.write_waypoint.v1','esi-location.read_online.v1','esi-clones.read_implants.v1'],
         userAgent: 'express 4.9.5, eve-sso, tdf-incursionauth 1.0',
         baseSSOUrl: "login.eveonline.com"
